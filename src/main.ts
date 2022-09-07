f55eca87de820be2142c0c9a95281af62f3a84d7
baha-nouaili <b.noueili@gmail.com>
2022-09-07 13:35:58 +0100

    swagger init

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { ConfigService } from '@nestjs/config';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('api');
+
+  const config = new DocumentBuilder()
+    .setTitle('Notes App')
+    .setDescription('Description') //TODO
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
 
   const port = new ConfigService().get<string>('PORT') || 4000;
   await app.listen(port);
