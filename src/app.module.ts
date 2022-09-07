f9a239cbd89963b575f5498e6dfaa74daf06fb06
baha-nouaili <b.noueili@gmail.com>
2022-09-07 14:05:17 +0100

    Mongodb init

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,24 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), UsersModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    UsersModule,
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (config: ConfigService) => {
+        const mongodbUri = config.get<string>('MONGODB_URI');
+        const dbName = config.get<string>('DB_NAME');
+        return {
+          uri: `${mongodbUri}/${dbName}`,
+        };
+      },
+    }),
+  ],
   controllers: [],
   providers: [],
 })
