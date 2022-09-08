acb14d7bdce4ee9a31bcb534e95f9efc7fa7c04d
baha-nouaili <b.noueili@gmail.com>
2022-09-08 13:30:21 +0100

    middleware and accesstoken validation setup

--- jwt_certs/gen.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-openssl genrsa -out private.pem 3072
-echo 'private key generated successfully'
-openssl rsa -in private.pem -pubout -out public.pem
-echo 'public key generated successfully'
